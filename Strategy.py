01125833f6750e320158af67785f157e982926c8
Alex Leska <deskmicealex@gmail.com>
2019-09-21 06:46:42 -0500

    Start to movement - not working correctly

--- Strategy.py
+++ Strategy.py
@@ -1,6 +1,7 @@
 from API import Game
 import random
 
+
 class Strategy(Game):
     """
         FILL THIS METHOD OUT FOR YOUR BOT:
@@ -53,11 +54,32 @@ class Strategy(Game):
         random.shuffle(order)
         decision = []
         unitId = 0
+        blockedTiles = self.get_blocks()
+
+        gradient = self.flatten_2d_array(self.gen_atk_tileset_all())
+
+        greater_than_1 = list(filter(lambda tile: tile[0] > 1, gradient))
+
+        greater_than_1.sort(key=self.take_threat)
+
         for i in order:
+            movement = ["DOWN"]*my_units[unitId].speed
+            bestBackupMove = ["DOWN"]*my_units[unitId].speed
+            for tile in greater_than_1:
+                path = self.path_to(
+                    (my_units[unitId].pos.x, my_units[unitId].pos.y), (tile[1], tile[2]), blockedTiles)
+                if path != None:
+                    bestBackupMove = path[0: my_units[unitId].speed]
+                if path != None and len(path) <= my_units[unitId].speed:
+                    movement = path
+                    while len(movement) < my_units[unitId].speed:
+                        movement.append("STAY")
+            if len(movement) == 0:
+                movement = bestBackupMove
             decision.append(
                 {
                     "priority": i + 1,
-                    "movement": ["DOWN"]*my_units[unitId].speed,
+                    "movement": movement,
                     "attack": "DOWN",
                     "unitId": my_units[unitId].id
                 }
@@ -65,45 +87,69 @@ class Strategy(Game):
             unitId += 1
         return decision
 
+    def take_threat(self, elem):
+        return elem[0]
+
+    def flatten_2d_array(self, arr2d):
+        arr1d = []
+        for arr in arr2d:
+            for element in arr:
+                arr1d.append(element)
+        return arr1d
+
+    def get_blocks(self):
+        tiles_to_avoid = []
+        for i in range(12):
+            for j in range(12):
+                tile = self.get_tile((i, j))
+                if tile.type != 'BLANK':
+                    tiles_to_avoid.append((i, j))
+        return tiles_to_avoid
+
     """
     Returns a 2d int array showing how close to the edge of a single unit's "offensive range" each tile is.
     Does not account for attack patterns or obstacles.
     """
 
     def gen_atk_tileset_single(self, unit):
-        
+
         tileset = []                                        # the 2d tileset returned
-        tilesetRow = []                                     # a temp storage for each row of tileset
-        atkRange = 4 + unit["speed"]                        # the range the unit can theoretically attack on their turn
+        # a temp storage for each row of tileset
+        tilesetRow = []
+        # the range the unit can theoretically attack on their turn
+        atkRange = 4 + unit.speed
 
         for i in range(12):
             for j in range(12):
 
                 x = j                                       # x is left to right
-                y = (11 - i)                                # y is bottom to top
-                
-                xDist = abs(unit["pos"][0] - x)
-                yDist = abs(unit["pos"][1] - y)
+                # y is bottom to top
+                y = (11 - i)
+
+                xDist = abs(unit.pos.x - x)
+                yDist = abs(unit.pos.y - y)
 
                 dist = xDist + yDist
 
-                tilesetRow.append(max(0, atkRange - dist))  # if the tile can be attacked by the unit in the next turn, it is nonzero
+                # if the tile can be attacked by the unit in the next turn, it is nonzero
+                tilesetRow.append((max(0, atkRange - dist), x, y))
 
             tileset.append(tilesetRow)
-        
+
         return tileset
 
     """
     Returns a 2d int array showing how close to the edge of all enemy units' "offensive range" each tile is.
     Does not account for attack patterns or obstacles.
     """
+
     def gen_atk_tileset_all(self):
         tileset3D = []
-        
+
         for unit in self.get_enemy_units():
-            if unit["hp"] > 0:
-                tileset3D.append(gen_atk_tileset_single(unit))
-        
+            if unit.hp > 0:
+                tileset3D.append(self.gen_atk_tileset_single(unit))
+
         tilesetFinal = []
         tilesetRowFinal = []
 
@@ -111,15 +157,8 @@ class Strategy(Game):
             for j in range(12):
                 maxThreat = 0
                 for k in range(len(tileset3D)):
-                    maxThreat = max(maxThreat, tileset3D[k][i][j])
-                tilesetRowFinal.append(maxThreat)
+                    maxThreat = max(maxThreat, tileset3D[k][i][j][0])
+                tilesetRowFinal.append((maxThreat, i, j))
             tilesetFinal.append(tilesetRowFinal)
-        
-        return tilesetFinal
 
-    #def move_bot(self, myUnit):
-        
-        # Default movement algorithm:
-        # If it is possible to move to a more advantageous combat position, move there
-        # Otherwise, if it is possible to move closer to the enemy, move there
-        # Otherwise, hold still
+        return tilesetFinal
